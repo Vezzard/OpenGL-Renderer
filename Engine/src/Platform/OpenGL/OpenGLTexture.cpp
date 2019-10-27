26875247e657216890379d2d76a39e0886b7ea8e
Vezzard <artvoz9111@gmail.com>
2019-10-27 21:36:38 +0300

    enable mipmapping

--- Engine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Engine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -54,8 +54,10 @@ namespace Engine {
 		glGenTextures(1, &m_RendererID);
 		glBindTexture(GL_TEXTURE_2D, m_RendererID);
 		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, GL_MAX_TEXTURE_MAX_ANISOTROPY);
 	}
 
 	void OpenGLTexture2D::Bind(uint slot) const
